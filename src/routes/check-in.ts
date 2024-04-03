dc7280a0de33ed26761cfa01c46e19f205aa9ae4
Kaue Alves <kaue_alves00@yahoo.com>
2024-04-03 09:44:59 -0300

    feat: Route - Check-in

---
+++ src/routes/check-in.ts
@@ -0,0 +1,46 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { prisma } from '../lib/prisma'
+
+export async function checkIn(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/attendees/:attendeeId/check-in',
+    {
+      schema: {
+        params: z.object({
+          attendeeId: z.coerce.number().int(),
+        }),
+        response: {
+          201: z.null(),
+          401: z.object({
+            message: z.string(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { attendeeId } = request.params
+
+      const attendeeCheckIn = await prisma.checkIn.findFirst({
+        where: {
+          attendeeId,
+        },
+      })
+
+      if (attendeeCheckIn) {
+        return reply
+          .status(401)
+          .send({ message: 'Attendee already checked in.' })
+      }
+
+      await prisma.checkIn.create({
+        data: {
+          attendeeId,
+        },
+      })
+
+      return reply.status(201).send()
+    },
+  )
+}
