43cd325651b4fa480866346cf9bbf0f20bd25714
Kaue Alves <kaue_alves00@yahoo.com>
2024-04-03 10:50:09 -0300

    docs: Swagger - doc api

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import fastify from 'fastify'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
+
 import {
   serializerCompiler,
   validatorCompiler,
+  jsonSchemaTransform,
 } from 'fastify-type-provider-zod'
 
 import { createEvent } from './routes/create-event'
@@ -12,6 +16,24 @@ import { checkIn } from './routes/check-in'
 import { getEventAttendees } from './routes/get-event-attendees'
 
 const app = fastify()
+
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    info: {
+      title: 'Pass.in',
+      description:
+        'Pass.in é uma API para gestão de participantes em eventos presenciais.',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
