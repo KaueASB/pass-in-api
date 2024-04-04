36d5a4c941ad5c7c3d04dc6bf1f661df4c25472b
Kaue Alves <kaue_alves00@yahoo.com>
2024-04-04 11:08:19 -0300

    feat: Seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,15 +1,55 @@
+import { Prisma } from '@prisma/client'
 import { prisma } from '../src/lib/prisma'
+import { faker } from '@faker-js/faker'
+import dayjs from 'dayjs'
 
 async function seed() {
+  const eventId = '7ee86dae-a0b9-40a7-8a2c-3260cbceabc0'
+
+  await prisma.event.deleteMany()
+
   await prisma.event.create({
     data: {
-      id: '7ee86dae-a0b9-40a7-8a2c-3260cbceabc0',
+      id: eventId,
       title: 'Seed Event',
       slug: 'seed-event',
       details: 'This is an seed event.',
       maximumAttendees: 100,
     },
   })
+
+  const attendeesToInsert: Prisma.AttendeeUncheckedCreateInput[] = []
+
+  for (let i = 0; i <= 120; i++) {
+    attendeesToInsert.push({
+      id: 10000 + i,
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      eventId,
+      createdAt: faker.date.recent({
+        days: 30,
+        refDate: dayjs().subtract(8, 'days').toDate(),
+      }),
+      checkIn: faker.helpers.arrayElement<
+        Prisma.CheckInUncheckedCreateNestedOneWithoutAttendeeInput | undefined
+      >([
+        undefined,
+        {
+          create: {
+            createdAt: faker.date.recent({ days: 7 }),
+          },
+        },
+      ]),
+    })
+  }
+
+  await Promise.all(
+    attendeesToInsert.map((data) => {
+      return prisma.attendee.create({
+        data,
+      })
+    }),
+  )
 }
 
 seed().then(() => {
